6cdb0c1a6c7956e49f6a2e69a27f6f6f83575287
geraldbryan <geraldbryan9914@gmail.com>
2024-06-27 16:24:58 +0700

    add another conversion rate

--- main.py
+++ main.py
@@ -6,10 +6,35 @@ import requests
 
 def get_conversion_rate():
     conversion_rate = {}
+    
     response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
-    rate_data = response.json()
-    usd_idr_rate = rate_data["rates"]["IDR"]
-    conversion_rate["USD"] = {"IDR": usd_idr_rate}
+    usd_idr_rate = response.json()["rates"]["IDR"]
+    usd_sgd_rate = response.json()["rates"]["SGD"]
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/AUD")
+    aud_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/EUR")
+    eur_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/GBP")
+    gbp_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/HKD")
+    hkd_sgd_rate = response.json()["rates"]['SGD']
+
+
+    conversion_rate["USD"] = {"IDR": usd_idr_rate,
+                          "SGD":usd_sgd_rate,}
+
+    conversion_rate["AUD"] = {"SGD":aud_sgd_rate}
+
+    conversion_rate["EUR"] = {"SGD":eur_sgd_rate}
+
+    conversion_rate["GBP"] = {"SGD":gbp_sgd_rate}
+
+    conversion_rate["HKD"] = {"SGD":hkd_sgd_rate}
+
 
     dt_now = datetime.datetime.now()
     conversion_rate["datetime"] = dt_now.strftime("%Y-%m-%d %H:%M:%S")
@@ -17,7 +42,7 @@ def get_conversion_rate():
     return conversion_rate
 
 
-if __name__ == "__main__":
-    conversion_rate = get_conversion_rate()
-    with open("conversion_rate.json", "w") as outfile:
-        json.dump(conversion_rate, outfile)
+# if __name__ == "__main__":
+#     conversion_rate = get_conversion_rate()
+#     with open("conversion_rate.json", "w") as outfile:
+#         json.dump(conversion_rate, outfile)
