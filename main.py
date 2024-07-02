0dc2f45ca5e2c4a8090bb47f6b36841fe1f35e49
geraldbryan <geraldbryan9914@gmail.com>
2024-07-02 15:59:43 +0700

    add some conversion rate

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ def get_conversion_rate():
     response = requests.get("https://api.exchangerate-api.com/v4/latest/HKD")
     hkd_sgd_rate = response.json()["rates"]['SGD']
 
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/CNY")
+    cny_sgd_rate = response.json()["rates"]['SGD']
+    cny_myr_rate = response.json()["rates"]['MYR']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/IDR")
+    idr_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/MYR")
+    myr_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/PHP")
+    php_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/THB")
+    thb_sgd_rate = response.json()["rates"]['SGD']
+
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/TWD")
+    twd_sgd_rate = response.json()["rates"]['SGD']
+
 
     conversion_rate["USD"] = {"IDR": usd_idr_rate,
                           "SGD":usd_sgd_rate,
@@ -36,6 +55,19 @@ def get_conversion_rate():
     conversion_rate["GBP"] = {"SGD":gbp_sgd_rate}
 
     conversion_rate["HKD"] = {"SGD":hkd_sgd_rate}
+
+    conversion_rate["CNY"] = {"SGD":cny_sgd_rate,
+                              "MYR":cny_myr_rate}
+
+    conversion_rate["IDR"] = {"SGD":idr_sgd_rate}
+
+    conversion_rate["MYR"] = {"SGD":myr_sgd_rate}
+
+    conversion_rate["PHP"] = {"SGD":php_sgd_rate}
+
+    conversion_rate["THB"] = {"SGD":thb_sgd_rate}
+
+    conversion_rate["TWD"] = {"SGD":twd_sgd_rate}
 
 
     dt_now = datetime.datetime.now()
